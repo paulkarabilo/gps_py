eb5418eb0ede9ba1128941138e2ad560e16ffff6
Paul Karabilo <benizebe@gmail.com>
2015-07-15 21:07:49 +0200

    at least something

--- gps.py
+++ gps.py
@@ -1,14 +1,38 @@
-import gtk
+from gi.repository import Gtk
 
-class GPSApp(gtk.Window):
+from processes import ProcessList
+
+class GnomePSWindow(Gtk.Window):
+
     def __init__(self):
-        super(GPSApp, self).__init__()
+        Gtk.Window.__init__(self, title="CellRendererText Example")
 
-        self.connect("destroy", gtk.main_quit)
-        self.set_size_request(250, 150)
-        self.set_position(gtk.WIN_POS_CENTER)
-        self.show()
+        self.set_default_size(200, 200)
+        self.processes = ProcessList()
+        self.processes.read()
+
+        self.liststore = Gtk.ListStore(str, str)
+        for proc in self.processes.list():
+            print "{0} {1}".format(proc.pid, proc.cmdline)
+            self.liststore.append([proc.pid, proc.cmdline])
+
+        treeview = Gtk.TreeView(model=self.liststore)
+
+        pid_text = Gtk.CellRendererText()
+        pid_col = Gtk.TreeViewColumn("PID", pid_text, text=0)
+        pid_col.set_max_width(300)
+        treeview.append_column(pid_col)
+
+        path_text = Gtk.CellRendererText()
+        path_col = Gtk.TreeViewColumn("Path", path_text, text=1)
+        path_col.set_max_width(300)
+        treeview.append_column(path_col)
+
+        self.add(treeview)
 
 
-GPSApp()
-gtk.main()
+
+win = GnomePSWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
---
+++ processes.py
@@ -0,0 +1,30 @@
+import os
+
+class Process:
+    def __init__(self, pid, cmdline, status):
+        self.pid = pid
+        self.cmdline = cmdline
+        self.status = status
+
+class ProcessList:
+    def __init__(self):
+        self.processes = {}
+
+    def read(self):
+        pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+        for pid in pids:
+            try:
+                cmdline = open(os.path.join('/proc', pid, 'cmdline'),
+                        'rb').read()
+                status = open(os.path.join('/proc', pid, 'status'),
+                        'rb').read()
+            except IOError:
+                continue
+
+            if cmdline:
+                self.processes[pid] = Process(pid, cmdline, status)
+
+    def list(self):
+        for pid in self.processes:
+            yield self.processes[pid]
+
