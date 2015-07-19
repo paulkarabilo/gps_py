9243701add83a8f08ce14b86e08615803621fd8d
Paul Karabilo <benizebe@gmail.com>
2015-07-19 13:46:05 +0200

    - error message on kill process - new process dialog

--- gps.py
+++ gps.py
@@ -4,10 +4,24 @@ import os, signal
 
 from processes import ProcessList
 
+class NewProcessDialog(Gtk.Dialog):
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, "Start new process", parent, 0,
+                            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+
+        self.set_default_size(150, 100)
+        self.entry = Gtk.Entry()
+        self.get_content_area().add(self.entry)
+        self.show_all()
+
+    def get_procname(self):
+        return self.entry.get_text()
+
 class GnomePSWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="CellRendererText Example")
+        Gtk.Window.__init__(self, title="Gnome process explorer")
         self.set_border_width(10)
         self.set_default_size(200, 200)
         self.set_position(Gtk.WindowPosition.CENTER)
@@ -41,7 +55,7 @@ class GnomePSWindow(Gtk.Window):
 
         self.new_button = Gtk.Button("New Process")
         self.kill_button = Gtk.Button("Kill Process")
-
+        self.new_button.connect("clicked", self.on_new_button_clicked)
         self.kill_button.connect("clicked", self.on_kill_button_clicked)
 
         self.grid.attach_next_to(self.new_button, self.scrollable,
@@ -63,16 +77,36 @@ class GnomePSWindow(Gtk.Window):
         model, treeiter = selection.get_selected()
         if treeiter is not None:
             self.selected_pid = model[treeiter][0]
-            print "You selected {0}".format(model[treeiter][0])
 
-    def on_new_button_clicked(self):
-        pass
+    def on_new_button_clicked(self, button):
+        dialog = NewProcessDialog(self)
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            procname = dialog.get_procname()
+            os.system(procname)
+            self.populate_proc_list()
+        elif response == Gtk.ResponseType.CANCEL:
+            pass
+
+        dialog.destroy()
 
     def on_kill_button_clicked(self, button):
         if self.selected_pid:
-            os.kill(int(self.selected_pid), signal.SIGKILL)
-            self.populate_proc_list()
-            self.selected_pid = None
+            try:
+                os.kill(int(self.selected_pid), signal.SIGKILL)
+                self.populate_proc_list()
+                self.selected_pid = None
+            except OSError:
+                self.show_error("Error", "Could not kill process {0}".format(
+                    self.selected_pid))
+
+    def show_error(self, header="Error", text=""):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR,
+                                 Gtk.ButtonsType.OK, header)
+        dialog.format_secondary_text(text)
+        dialog.run()
+        dialog.destroy()
 
 win = GnomePSWindow()
 win.connect("delete-event", Gtk.main_quit)
