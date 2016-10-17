334cdf88ea31da49074d9ef59090381a17ed82e8
Paul Karabilo <benizebe@gmail.com>
2016-10-17 22:53:33 +0200

    some initial cpuinfo blablabla

--- gps.py
+++ gps.py
@@ -1,6 +1,5 @@
 import gi
 gi.require_version('Gtk', '3.0')
-
 from gps.application import Application
 from signal import signal, SIG_DFL, SIGINT
 import sys
--- gps/cpuinfo.py
+++ gps/cpuinfo.py
@@ -1,15 +1,25 @@
 from os import open
 
 class CPU:
-    def __init__(self):
-        pass
+    def __init__(self, number):
+        self.params = {"n": number}
 
+    def set(self, param, value):
+        self.params[param] = value
 
 class CpuInfo:
     def __init__(self):
         self.cpus = {}
 
     def read(self):
+        cur_proc = None
         with open("/proc/cpuinfo") as f:
             for l in f:
-                print l
+                [param, value] = l.split(':')
+                param = param.strip()
+                name = name.strip()
+                if param is "processor":
+                    self.cpus[value] = CPU(value) if value not in self.cpus else self.cpus[value]
+                    cur_proc = value
+                else:
+                    self.cpus[cur_proc].set(param, value)
